68742102c82631d8126cdcfca81d56fa341b0e2b
joshwayman <nowayman121@gmail.com>
2022-04-29 13:20:05 +1000

    Fix

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ function App() {
         break;
     }
 
-  }, [activeSig, name, title, mobile, img, lineColor, logo, fullURL, website, linkedIn, officePhone, email,]);
+  }, [activeSig, name, title, mobile, img, lineColor, logo, fullURL, website, linkedIn, officePhone, email, address]);
 
 
 
